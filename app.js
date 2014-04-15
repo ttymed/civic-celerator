55c0b9e82491f1409664a17ae1e43c795048820e
Tyler Boright <WuKong@Tylers-MacBook-Air.local>
2014-04-14 18:26:04 -1000

    created mongoose schema stub for candidate and established mongodb connection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var server = restify.createServer();
 server.use(restify.bodyParser());
 var mongoose = require('mongoose/');
-var config = require('./config');
+// var config = require('./config');
 db = mongoose.connect('mongodb://localhost/text');
 Schema = mongoose.Schema;
 
@@ -11,6 +11,36 @@ Schema = mongoose.Schema;
 var CandidateSchema = new Schema({
   name: String
 });
+
+
+//create candidate model in mongodb
+mongoose.model('Candidate', CandidateSchema);
+var Candidate = mongoose.model('Candidate');
+
+
+//get all candidates
+function getCandidates(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //.find() without arguments to return all candidates
+  //-1 to denote descending order
+  Candidate.find().exec(function (arr,data) {
+    res.send(data);
+  });
+}
+
+// function findOneCandidate(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   Candidate.findOne(_id: _id)
+
+// }
+
+
+//rest verb handling
+server.get('/candidates', getCandidates);
+
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
